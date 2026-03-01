657c50601dd417068db2e4fefe1657daf2208b3d
Kedolce <Kedolcegocek@gmail.com>
2026-03-01 14:39:31 +0700

    Simple To Do List With Indexing Delete

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputText = document.querySelector("#inputCatatan");
+const tombolTambah = document.querySelector("#tombolTambah");
+const daftarTugas = document.querySelector("#daftarCatatan");
+const inputHapus = document.querySelector("#inputDelete");
+const tombolHapus = document.querySelector("#deleteButton");
+
+tombolTambah.addEventListener("click", function () {
+  const isiCatatan = inputText.value;
+
+  if (isiCatatan === "") {
+    alert("Fill something please...");
+    return;
+  }
+
+  const listBaru = document.createElement("li");
+  listBaru.innerText = isiCatatan;
+
+  daftarTugas.appendChild(listBaru);
+
+  inputText.value = "";
+});
+
+tombolHapus.addEventListener("click", function () {
+    const inputNomorDel = parseInt(inputHapus.value);
+    const daftarLi = daftarTugas.children;
+
+    if(inputNomorDel > 0 && inputNomorDel <= daftarLi.length){
+        daftarLi[inputNomorDel - 1].remove();
+        inputHapus.value = "";
+        return;
+    }
+    else{
+        alert("Nomor tidak valid");
+        return;
+    }
+  });
